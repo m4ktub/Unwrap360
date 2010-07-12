f568db758772b60e5b3acda88eb02e213985b512
claudio.gil <claudio.gil@shaman.m4ktub.vilt-group.com>
2010-07-12 23:04:36 +0100

    revised the debian package system and removed example files

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the unwrap360 package
-#
-0 4	* * *	root	[ -x /usr/bin/unwrap360_maintenance ] && /usr/bin/unwrap360_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(unwrap360):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="unwrap360" command="/usr/bin/unwrap360"
